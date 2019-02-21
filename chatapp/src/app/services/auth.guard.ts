a2dd8fe9cbdd7066135b48874e1e08aba754d9e3
typescript59 <typescript59@gmail.com>
2019-02-21 13:17:54 +0530

    Auth Guard

--- chatapp/src/app/services/auth.guard.ts
+++ chatapp/src/app/services/auth.guard.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private tokenService: TokenService) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    // return true;
+
+    const token = this.tokenService.getToken();
+    if (token) {
+      return true;
+    } else {
+      this.router.navigate(['/']);
+      return false;
+    }
   }
 }
