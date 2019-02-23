8208f0cbd8c2c410f91c97fceef9ca6e8e480202
typescript59 <typescript59@gmail.com>
2019-02-23 15:57:31 +0530

    Display Username On Toolbar

--- chatapp/src/app/components/toolbar/toolbar.component.ts
+++ chatapp/src/app/components/toolbar/toolbar.component.ts
@@ -8,9 +8,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
+  user: any;
   constructor(private tokenService: TokenService, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.user = this.tokenService.GetPayloadOfToken();
+  }
 
   logout() {
     this.tokenService.deleteToken();
