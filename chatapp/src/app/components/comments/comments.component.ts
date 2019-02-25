ad3ac8d56fb00a0b5f487db426076fb4d376c9fe
typescript59 <typescript59@gmail.com>
2019-02-25 19:51:49 +0530

    Comments Service Method

--- chatapp/src/app/components/comments/comments.component.ts
+++ chatapp/src/app/components/comments/comments.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PostService } from 'src/app/services/post.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-comments',
@@ -7,14 +10,32 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 })
 export class CommentsComponent implements OnInit, AfterViewInit {
   toolbarElement: any;
+  commentForm: FormGroup;
+  postId: any;
 
-  constructor() {}
+  constructor(private fb: FormBuilder, private postService: PostService, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.postId = this.route.snapshot.paramMap.get('id');
     this.toolbarElement = document.querySelector('.nav-content');
+    this.init();
+  }
+
+  init() {
+    this.commentForm = this.fb.group({
+      comment: ['', Validators.required]
+    });
   }
 
   ngAfterViewInit() {
     this.toolbarElement.style.display = 'none';
+  }
+
+  AddComment() {
+    // console.log(this.commentForm.value);
+
+    this.postService.addComment(this.postId, this.commentForm.value.comment).subscribe(data => {
+      console.log(data);
+    });
   }
 }
